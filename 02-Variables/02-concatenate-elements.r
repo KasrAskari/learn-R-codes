3e002cc6d96bdd6245353668c7407426a4274e17
Kasra Askarizadeh <kasra.askarizadeh2000@gmail.com>
2025-02-09 00:44:34 +0330

    Add files via upload

---
+++ 02-Variables/02-concatenate-elements.r
@@ -0,0 +1,19 @@
+text <- "awsome"
+paste("R is", text)
+
+text1 <- "R is"
+text2 <- "awsome"
+
+paste(text1, text2)
+
+
+num1 <- 5
+num2 <- 10
+
+num1 + num2
+
+
+# num <- 5
+# text <- "some text"
+
+# num + text
---
+++ 04-Numbers/03-complex.r
@@ -0,0 +1,8 @@
+x <- 3+5i
+y <- 5i
+
+x
+y
+
+class(x)
+class(y)
