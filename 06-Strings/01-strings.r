63652c74560a28bf48bbed15c62e83f62293b682
Kasra Askarizadeh <kasra.askarizadeh2000@gmail.com>
2025-02-11 19:57:05 +0330

    Add files via upload

---
+++ 06-Strings/01-strings.r
@@ -0,0 +1,36 @@
+"hello"
+'hello'
+
+
+str <- "Hello"
+str # print the value of str
+
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+str # print the value of str
+
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+cat(str)
+
+
+str <- "Hello World!"
+
+nchar(str)
+grepl("H", str)
+grepl("Hello", str)
+grepl("X", str)
+
+
+str1 <- "Hello"
+str2 <- "world"
+
+paste(str1, str2)
---
+++ 09-If-Else/03-and-or.r
@@ -0,0 +1,16 @@
+a <- 200
+b <- 33
+c <- 500
+
+if (a > b && c > a) {
+  print("Both conditions are true")
+}
+
+
+a <- 200
+b <- 33
+c <- 500
+
+if (a > b || a > c) {
+  print("At least one of the conditions is true")
+}
