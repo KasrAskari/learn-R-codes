421c7d705cc9bce594cb5a70480e4ea6db60387e
Kasra Askarizadeh <kasra.askarizadeh2000@gmail.com>
2025-03-01 00:53:24 +0330

    Add files via upload

--- Data-Import-Export/01-importing-data.r
+++ Data-Import-Export/01-importing-data.r
@@ -1,11 +1,11 @@
-# Read a CSV file into R
+# 1- Read a CSV file into R
 data_csv <- read.csv("data.csv", header = TRUE, sep = ",")
 
 # Display the first few rows of the dataset
 head(data_csv)
 
 
-# Read Excel File(useing readxl)
+# 2- Read Excel File(useing readxl)
 
 install.packages("readxl") #install the pakage (only needed ones)
 library(readxl)
@@ -16,7 +16,7 @@ data_excel <- read_excel("data.xlsx", sheet = 1)
 head(data_excel)
 
 
-# Read JSON File (useing jsonlite)
+# 3- Read JSON File (useing jsonlite)
 
 install.packages("jsonlite") #install the pakage (only needed ones)
 library(jsonlite)
@@ -28,12 +28,12 @@ data_json <- fromJSON("data.json")
 print(data_json)
 
 
-# Read Data from an SQL Database (Using RSQLite)
+# 4- Read Data from an SQL Database (Using RSQLite)
 
 install.packages("RSQLite")
 library(RSQLite)
 
-# Connect to an SQLite database
+# 5- Connect to an SQLite database
 conn <- dbConnect(SQLite(), "database.sqlite")
 
 # Read data from a table
@@ -46,7 +46,7 @@ dbDisconnect(conn)
 head(data_sql)
 
 
-# Read Data from the Web (CSV Format)
+# 6- Read Data from the Web (CSV Format)
 
 # Read a CSV file directly from a URL
 url <- "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
